eeb1c4330024e96513c4aba62c7cd3bebc406cf0
BanuKarakaya <nurbanukarakaya03@gmail.com>
2025-03-29 22:48:00 +0300

    first commit for WaterBoost

---
+++ WaterBoost/WaterBoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterBoostApp.swift
+//  WaterBoost
+//
+//  Created by Banu on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct WaterBoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
