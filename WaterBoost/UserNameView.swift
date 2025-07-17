358a65a4c528ba3e0ea0a9846b4d6c74a49230fd
BanuKarakaya <nurbanukarakaya03@gmail.com>
2025-07-17 19:22:52 +0300

    finishing the username page

--- WaterBoost/UserNameView.swift
+++ WaterBoost/UserNameView.swift
@@ -5,4 +5,56 @@
 //  Created by Banu on 17.07.2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct UserNameView: View {
+    
+    @State private var isAnimating: Bool = false
+    let darkBlue = Color(red: 0/255, green: 27/255, blue: 43/255)
+    @State var text = ""
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                Image("group")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 250, maxHeight: 250)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
+                
+                Text("Can you share your name with us?")
+                    .foregroundColor(Color.white)
+                    .font(.title2)
+                    .frame(maxWidth: 450, alignment: .center)
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.bold)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                TextField("Enter Your Name", text: $text)
+                    .padding()
+                    .frame(maxWidth: 250, alignment: .center)
+                    .background(.white)
+                    .foregroundStyle(darkBlue)
+                    .clipShape(RoundedRectangle(cornerRadius: 7))
+                
+                StartButtonView()
+            }
+        }
+        .onAppear {
+            withAnimation(.easeOut(duration: 0.5)) {
+                  isAnimating = true
+            }
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [darkBlue, Color.blue]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        .padding(.horizontal, 20)
+    }
+}
+
+#Preview {
+    UserNameView()
+}
+
+
